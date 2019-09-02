4e87666d5ede4a6616d001924bd7636966211adf
Taylor Covington <tcovingtondev@gmail.com>
2019-09-02 14:28:14 -0600

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'notes#index'
+
+  get '/about', to: 'static_pages#about'
+
   resources :notes
+
 end
